b55dc235318a0218397fcb58e002586928fac016
Ian Pascoe <ian.g.pascoe@gmail.com>
2026-04-10 09:18:07 -0400

    chore: update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,10 @@
 vim.uv = vim.uv or vim.loop
 
 -- Global Utilities
-Util = require("util")
+Util = require('util')
 
 -- Enable the module loader
 vim.loader.enable()
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require('config.lazy')
